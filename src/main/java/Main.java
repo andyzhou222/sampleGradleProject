45f2e65acb5a73d7725babf363a7685ce7dac2ba
andy zhou <andy zhou>
2017-02-26 22:05:12 +0800

    my first commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,20 @@
+import javax.swing.*;
+
 public class Main {
-	public static void main(String[] args) {
-		System.out.println("Yo what's up human! Oh you think you're fancy");
-	}
+    public static void main(String[] args) {
+        System.out.print("20");
+        int answer = (int)(Math.random()*100)+1;
+        int input = 0;
+        String show = JOptionPane.showInputDialog("");
+        input = Integer.parseInt(show);
+        while(input != answer){
+            if(input > answer){
+                show = JOptionPane.showInputDialog("too big guess again");
+                input = Integer.parseInt(show);
+            }else if(input < answer){
+                show = JOptionPane.showInputDialog("too small guess agian");
+                input = Integer.parseInt(show);
+            }
+        }System.out.print("you are right!");
+    }
 }
